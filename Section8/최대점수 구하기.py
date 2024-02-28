74a86f51f0e10742faf5492bd3a7d7583fef7c57
tkv00 <144890194+tkv00@users.noreply.github.com>
2024-02-28 20:14:05 +0900

    최대 점수 구하기

--- Section8/최대점수 구하기.py
+++ Section8/최대점수 구하기.py
@@ -1,26 +1,13 @@
 import sys
-sys.stdin=open("/Users/kimdoyeon/Desktop/pythonalgorithm_formac/섹션 8/11. 최대점수 구하기(냅색알고리즘)/in1.txt")
-
-def DFS(L,t):
-    global sum
-    if t>time:
-        return 
-    else:
-        DFS(L+1,t+a[L][1])
-        dp[L]=dp[L-1]+a[L][0]
-        sum=max(dp[L],sum)
-        DFS(L+1,t)
-        dp[L]=dp[L-1]
-
-
-if __name__=="__main__":
-    sol,time=map(int,input().split())
-    a=[]
-    sum=0
-    for i in range(sol):
-        k,t=map(int,input().split())
-        a.append((k,t))
-    dp=[0]*(time+1)
-    DFS(0,0)
-    print(sum)
-
+sys.stdin=open("/Users/kimdoyeon/Desktop/pythonalgorithm_formac/섹션 8/11. 최대점수 구하기(냅색알고리즘)/in5.txt")
+n,m=map(int,input().split())
+a=[]
+for i in range(n):
+    N,T=map(int,input().split())
+    a.append((N,T))
+    
+dp=[0]*(m+1)
+for i in range(n):
+    for j in range(m,a[i][1]-1,-1):
+        dp[j]=max(dp[j],a[i][0]+dp[j-a[i][1]])
+print(dp[m])

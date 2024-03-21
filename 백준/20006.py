d6ce15d4b64ff47072b6561ce966a37d7a011606
tkv00 <144890194+tkv00@users.noreply.github.com>
2024-03-21 17:35:42 +0900

    3월 21일

---
+++ 백준/20006.py
@@ -0,0 +1,7 @@
+level_num,id_num=map(int,input().split())
+a=dict()
+for _ in range(level_num):
+    num,id=input().split()
+    num=int(num)
+    a[id]=num
+
--- 백준/22233.py
+++ 백준/22233.py
@@ -1,9 +1,20 @@
-n,m=map(int,input().split())
-memo=[]
-blog=[]
-for _ in range(n):
-    memo.append(input())
-for _ in range(m):
-    blog.append(input().split(','))
+# 가희와 키워드
+import sys
 
-print(blog)
+key_n, post_n = map(int, sys.stdin.readline().split(' '))
+
+memo = {}
+answer = 0
+
+for _ in range(key_n):
+    memo[sys.stdin.readline().rstrip()] = 1
+    answer += 1
+
+for _ in range(post_n):
+    post = list(sys.stdin.readline().rstrip().split(','))
+    for i in post:
+        if i in memo.keys():
+            if memo[i] == 1:
+                memo[i] = 0
+                answer -= 1
+    print(answer)

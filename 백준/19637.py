16c98d4b000713fff34df664b4aa662b5b4f760d
tkv00 <144890194+tkv00@users.noreply.github.com>
2024-03-22 22:54:11 +0900

    3월 22일

---
+++ 백준/19637.py
@@ -0,0 +1,8 @@
+n,m=map(int,input().split())
+name=[]
+for _ in range(n):
+    a,b=input().split()
+    name.append(a,int(b))
+char=[]
+for _ in range(m):
+    char.append(int(input()))
--- 백준/20006.py
+++
@@ -1,7 +0,0 @@
-level_num,id_num=map(int,input().split())
-a=dict()
-for _ in range(level_num):
-    num,id=input().split()
-    num=int(num)
-    a[id]=num
-
